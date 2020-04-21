0d97368bd94520bda229ae0036ff8901d96db012
taypal <tay.houk@yahoo.com>
2020-04-20 21:45:42 -0600

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
-// import Header from "./Component/header";
-// import Navbar from "./Component/navbar";
+
 import About from "./Component/pages/about";
+import Portfolio from "./Component/pages/portfolio";
+import Contact from "./Component/pages/contact";
+import Resume from "./Component/pages/resume";
 
 
 function App() {
   return (
     <div className="parent">
 
-      <About />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/resume" component={Resume} />
 
     </div>
   )
